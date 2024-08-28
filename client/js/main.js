0740caeae19693efa63922facb315589ff32239b
danielace1 <danielsudharsan1805@gmail.com>
2024-08-28 22:07:34 +0530

    feat: fix cookie

--- client/js/main.js
+++ client/js/main.js
@@ -154,7 +154,7 @@ async function handleSubmit(e) {
     if (rememberMe) {
       document.cookie = `username=${username}; path=/; max-age=3600; Secure; SameSite=Lax`;
     } else {
-      document.cookie = `username=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+      document.cookie = `username=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure; SameSite=Lax`;
     }
 
     formEl.reset();
